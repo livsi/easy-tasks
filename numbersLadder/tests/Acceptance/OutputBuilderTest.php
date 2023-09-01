fdf3f8adf253f689d73f7ade2e9b60bfd1d09076
Lezhnev <Lezhnev.V.S@sblogistica.ru>
2023-09-01 17:00:32 +0300

    resolve task

---
+++ numbersLadder/tests/Acceptance/OutputBuilderTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Test\Acceptance;
+
+use App\OutputBuilder;
+use App\SimpleFormatter;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(OutputBuilder::class)]
+#[UsesClass(SimpleFormatter::class)]
+class OutputBuilderTest extends TestCase
+{
+    public static function cases(): iterable
+    {
+        yield '1'=> [1, 1, "1\n"];
+        yield '3 items'=> [1, 3, "1\n2 3\n"];
+        yield '7 items'=> [1, 7, "1\n2 3\n4 5 6\n7\n"];
+        yield '3 items in reverse order'=> [3, 1, "3\n2 1\n"];
+    }
+
+    #[DataProvider('cases')]
+    public function testGetResult(int $start, int $end, $expected)
+    {
+        self::assertEquals($expected, (new OutputBuilder())->getResult($start, $end, (new SimpleFormatter())()));
+    }
+
+
+
+}
