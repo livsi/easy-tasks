8467b2716c287884db79a624a373f03b01e249fd
Lezhnev <Lezhnev.V.S@sblogistica.ru>
2023-09-04 16:31:29 +0300

    fix tests, add RandomUniqueIntGeneratorFactoryTest

--- matrixWithRandomNumbers/tests/Acceptance/RandomUniqueIntGenerator/LotoTest.php
+++ matrixWithRandomNumbers/tests/Acceptance/RandomUniqueIntGenerator/LotoTest.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Matrix\Test\Acceptance\RandomUniqueIntGenerator;
 
-use Matrix\RandomUniqueIntegerGeneratorLogicException;
 use Matrix\RandomUniqueIntGenerator\Loto;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 use function PHPUnit\Framework\assertGreaterThanOrEqual;
@@ -25,19 +23,5 @@ final class LotoTest extends TestCase
             assertGreaterThanOrEqual($start, $number);
             assertLessThanOrEqual($end, $number);
         }
-    }
-
-    #[DataProvider('invalidProperties')]
-    public function testExceptionalOnCreateLoto($min, $max, $expectExc): void
-    {
-        $this->expectException($expectExc);
-        new Loto($min, $max);
-    }
-
-    public static function invalidProperties(): iterable
-    {
-        yield 'max < min' => [0, -1, RandomUniqueIntegerGeneratorLogicException::class];
-        yield 'out of range int' => [0, PHP_INT_MAX + 1, \TypeError::class];
-        yield 'max range int' => [0, 1073741823, RandomUniqueIntegerGeneratorLogicException::class];
     }
 }
