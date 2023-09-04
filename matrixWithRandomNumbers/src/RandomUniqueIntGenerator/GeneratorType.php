d37089f164a16addd7937cf82335a6f24c616d61
Lezhnev <Lezhnev.V.S@sblogistica.ru>
2023-09-04 16:29:03 +0300

    fix cs

--- matrixWithRandomNumbers/src/RandomUniqueIntGenerator/GeneratorType.php
+++ matrixWithRandomNumbers/src/RandomUniqueIntGenerator/GeneratorType.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Matrix\RandomUniqueIntGenerator;
 
-use \InvalidArgumentException;
-
 enum GeneratorType: string
 {
     case LOTO = Loto::class;
@@ -15,11 +13,11 @@ enum GeneratorType: string
     public static function tryFromName(string $name): self
     {
         foreach (self::cases() as $case) {
-            if (strtolower($case->name) === $name){
-                return  $case;
+            if (strtolower($case->name) === $name) {
+                return $case;
             }
         }
 
-        throw new InvalidArgumentException("Name $name not exist");
+        throw new \InvalidArgumentException("Name $name not exist");
     }
 }
