0bd3d9cd479ab44793f000996a519a2500b5caf7
Lezhnev <Lezhnev.V.S@sblogistica.ru>
2023-09-07 12:54:48 +0300

    add tests

--- matrixWithRandomNumbers/tests/Unit/RandomUniqueIntGenerator/RandomUniqueIntGeneratorFactoryTest.php
+++ matrixWithRandomNumbers/tests/Unit/RandomUniqueIntGenerator/RandomUniqueIntGeneratorFactoryTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Matrix\Test\Acceptance\RandomUniqueIntGenerator;
+namespace Matrix\Test\Unit\RandomUniqueIntGenerator;
 
 use Matrix\RandomUniqueIntegerGeneratorLogicException;
 use Matrix\RandomUniqueIntGenerator\GeneratorType;
@@ -14,20 +14,25 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(RandomUniqueIntGeneratorFactory::class)]
 class RandomUniqueIntGeneratorFactoryTest extends TestCase
 {
-    #[DataProvider('invalidProperties')]
-    public function testExceptionalCreate($type, int $min, mixed $max, string $expectExc): void
+    #[DataProvider('cases')]
+    public function testCreate($type, int $min, mixed $max, ?string $expectException): void
     {
         $factory = new RandomUniqueIntGeneratorFactory();
-        $this->expectException($expectExc);
-        $factory->create($type, $min, $max);
+        if ($expectException) {
+            $this->expectException($expectException);
+        }
+
+        $result = $factory->create($type, $min, $max);
+        self::assertTrue(method_exists($result, 'getNumber'));
     }
 
-    public static function invalidProperties(): iterable
+    public static function cases(): iterable
     {
         foreach (GeneratorType::cases() as $type) {
             yield 'max < min:'.$type->name => [$type, 0, -1, RandomUniqueIntegerGeneratorLogicException::class];
             yield 'out of range int:'.$type->name => [$type, 0, PHP_INT_MAX + 1, \TypeError::class];
             yield 'max range int:'.$type->name => [$type, 0, 1073741823, RandomUniqueIntegerGeneratorLogicException::class];
+            yield 'normal:'.$type->name => [$type, 0, 10, null];
         }
     }
 }
